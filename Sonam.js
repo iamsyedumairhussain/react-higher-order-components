294f232c3aeb911ad518d7da923bd65d4129c40e
Syed Umair Hussain <142947133+iamsyedumairhussain@users.noreply.github.com>
2024-12-05 18:02:30 +0500

    Update Sonam.js

--- Sonam.js
+++ Sonam.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import Army from './Army'
+import WithArm from './WithArm'
 
-const Sonam = ({name}) => {
-  return (
-    <div>Sonam {name}</div>
-  )
+function Sonam({shots, incrementShots, identifier, dept}) {
+    return (
+        <div>
+            <h1>Sonam : {shots}</h1>
+            <h1>Department : {dept}</h1>
+            <h1>identifier : {identifier}</h1>
+
+            <button onClick={incrementShots}>Shot </button>
+        </div>
+    )
 }
-
-export default Army(Sonam,700)
+export default WithArm(Sonam, 'sonam')
